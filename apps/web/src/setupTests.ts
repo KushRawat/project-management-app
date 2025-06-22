8d65042451965b5b7704ad139f28dc7aa5d022ff
Kush Rawat <rawatkush0704@gmail.com>
2025-06-22 20:25:04 +0530

    polyfill ResizeObserver & fix TSX JSX setting

--- apps/web/src/setupTests.ts
+++ apps/web/src/setupTests.ts
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom';
+
+// Polyfill ResizeObserver for JSDOM
+class ResizeObserver {
+  observe(_target: Element) {}
+  unobserve(_target: Element) {}
+  disconnect() {}
+}
+;(global as any).ResizeObserver = ResizeObserver;
+
